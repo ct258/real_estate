e07397124ca09aa071d32d91fd70bbf09626c0d5
Nghĩa Lê <46536254+nghiale1@users.noreply.github.com>
2020-01-14 09:55:26 +0700

    create project

---
+++ app/Models/KhachHang.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KhachHang extends Model
+{
+    protected $table = 'khachhang';
+
+    protected $primaryKey = 'kh_id';
+
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'kh_id',
+        'kh_hoten',
+        'kh_ngaysinh',
+        'kh_sdt',
+        'kh_diachi',
+        'kh_cmnd',
+        'kh_ngaycap',
+        'kh_noicap',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+    public $timestamps = true;
+}
