9e42f7504f79917628fe061a4bf81d02430dba6d
Nghĩa Lê <46536254+nghiale1@users.noreply.github.com>
2020-02-22 18:46:42 +0700

    #nghia.le thêm bảng mẫu

---
+++ app/Models/StandardPrice.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StandardPrice extends Model
+{
+    protected $table = 'standard_price';
+
+    protected $primaryKey = 'standard_price_id';
+
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'form_id',
+        'standard_price_id',
+        'standard_price_name',
+        'standard_price_value1',
+        'standard_price_value2',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public $timestamps = true;
+    protected $dates = ['deleted_at'];
+}
---
+++ database/migrations/2020_02_20_105441_create_standard_price_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStandardPriceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('standard_price')) {
+            Schema::create('standard_price', function (Blueprint $table) {
+                $table->increments('standard_price_id');
+                $table->string('standard_price_name')->nullable()->index()->comment('Chuẩn của giá');
+                $table->decimal('standard_price_value1', 18, 4)->nullable()->unsigned()->index()->comment('Chuẩn của giá');
+                $table->decimal('standard_price_value2', 18, 4)->nullable()->unsigned()->index()->comment('Chuẩn của giá');
+
+                //foreign key
+                $table->integer('form_id')->index()->unsigned();
+
+                $table->foreign('form_id')->references('form_id')->on('form');
+
+                //log time
+                $table->timestamp('created_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP'))
+                ->comment('ngày tạo');
+
+                $table->timestamp('updated_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+                ->comment('ngày cập nhật');
+
+                $table->timestamp('deleted_at')
+                ->nullable()
+                ->comment('ngày xóa tạm');
+            });
+            DB::statement("ALTER TABLE `standard` comment 'Tiêu chuẩn giá'");
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        // Schema::dropIfExists('standard_price');
+    }
+}
