dca5daf7dff6980e275e9d481c4e574609fa9917
Nghĩa Lê <46536254+nghiale1@users.noreply.github.com>
2020-03-04 22:04:12 +0700

    #nghia.le them cart_temp, sửa tên card->cart

---
+++ app/Models/CartTemp.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartTemp extends Model
+{
+    protected $table = 'cart_temp';
+
+    protected $primaryKey = 'cart_temp_id';
+
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'real_estate_id',
+        'cart_temp_id',
+        'cart_temp_cookie_name',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public $timestamps = true;
+}
