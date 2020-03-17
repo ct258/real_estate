ec95df3652a3e84305443c3d3dabf6bb4636acea
Tran Phung <tranthanhphungcttv@gmail.com>
2020-03-17 10:48:03 +0700

    #phung.tran create Search nâng cao
    update 17/3/2020  time: 10:47

--- resources/views/layouts/user/filter.blade.php
+++ resources/views/layouts/user/filter.blade.php
@@ -1,11 +1,25 @@
 <!-- Filter form section -->
 <div class="container">
     <div class="row search">
-        <div class="col-lg-8 search1">
+        <div class="col-lg-6 search1">
             <input class="form-control" placeholder="Tìm kiếm theo địa điểm, tên căn hộ hoặc từ khóa" id="search"
                 type="text" value="">
         </div>
-        <div class="col-lg-4 search1">
+        <div class="col-lg-2 select">
+            <select class="form-control ">
+                <option value="">cần mua</option>
+                <option>Thuê </option>
+                
+            </select>
+        </div>
+        <div class="col-lg-2 select left">
+            <select class="form-control">
+                <option>Loại</option>
+                <option>Chung Cư</option>
+                
+            </select>
+        </div>
+        <div class="col-lg-2 search1">
             <button class="btn btn-orange " type="submit">
                 Tìm Kiếm <i class="fa fa-search"></i>
             </button>
