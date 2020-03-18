f1d94f86700a7b8f414f03e2e85341074dc31c83
Nghĩa Lê <46536254+nghiale1@users.noreply.github.com>
2020-03-18 20:11:25 +0700

    #nghia.le color view user

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -4,23 +4,22 @@
         <div class="row">
             <div class="col-lg-3 col-md-6 footer-widget">
                 <img src="{{asset('leramiz/img/logo.png')}}" alt="">
-                <p>Lorem ipsum dolo sit azmet, consecter dipise consult elit. Maecenas mamus antesme non anean a dolor
-                    sample tempor nuncest erat.</p>
+                <p>Batdongsancantho là trang web chuyên kinh doanh bất động sản lớn nhất Cần Thơ.</p>
                 <div class="social">
-                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    {{-- <i href="#"><i class="fa fa-facebook"></i></i>
                     <a href="#"><i class="fa fa-twitter"></i></a>
                     <a href="#"><i class="fa fa-instagram"></i></a>
                     <a href="#"><i class="fa fa-pinterest"></i></a>
-                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a> --}}
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 footer-widget">
                 <div class="contact-widget">
                     <h5 class="fw-title">CONTACT US</h5>
-                    <p><i class="fa fa-map-marker"></i>3711-2880 Nulla St, Mankato, Mississippi </p>
-                    <p><i class="fa fa-phone"></i>(+88) 666 121 4321</p>
-                    <p><i class="fa fa-envelope"></i>info.leramiz@colorlib.com</p>
-                    <p><i class="fa fa-clock-o"></i>Mon - Sat, 08 AM - 06 PM</p>
+                    <p><i class="fa fa-map-marker"></i>Khu II, đường 3/2, P. Xuân Khánh, Q. Ninh Kiều, TP. Cần Thơ.</p>
+                    <p><i class="fa fa-phone"></i>(+12) 345 678 9999</p>
+                    <p><i class="fa fa-envelope"></i>realestatect258@gmail.com</p>
+                    <p><i class="fas fa-clock"></i>Mon - Sat, 08 AM - 06 PM</p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 footer-widget">
@@ -48,21 +47,27 @@
                     <p>Subscribe your email to get the latest news and new offer also discount</p>
                     <form class="footer-newslatter-form">
                         <input type="text" placeholder="Email address">
-                        <button><i class="fa fa-send"></i></button>
+                        <button><i class="fas fa-paper-plane" style="color:white"></i></button>
                     </form>
                 </div>
             </div>
         </div>
         <div class="footer-bottom">
             <div class="footer-nav">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Featured Listing</a></li>
-                    <li><a href="">About us</a></li>
-                    <li><a href="">Pages</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">Contact</a></li>
-                </ul>
+                <table style="color:white;">
+                    <tr>
+                        <td>Đang trực tuyến </td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: {{ $query_result_person[0]}}</td>
+                    </tr>
+                    <tr>
+                        <td>Lượt truy cập hôm nay </td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: {{ $query_result_person[1]}}</td>
+                    </tr>
+                    <tr>
+                        <td>Lượt truy cập </td>
+                        <td>&nbsp;&nbsp;&nbsp;&nbsp;: {{ $query_result_person[2]}}</td>
+                    </tr>
+                </table>
             </div>
             <div class="copyright">
                 <p>
@@ -71,6 +76,7 @@
                         document.write(new Date().getFullYear());
                     </script> All rights reserved | This template is made with <i class="fa fa-heart-o"
                         aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
             </div>
@@ -79,5 +85,5 @@
 </footer>
 <!-- Footer section end -->
 <div class="scrollupp">
-    <i id ='scrollupp'class="fa fa-angle-up"></i>
- </div>
+    <i id='scrollupp' class="fa fa-angle-up"></i>
+</div>
