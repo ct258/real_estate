e51439a00e9a3418076e82a7a0884f44642352ab
Nghĩa Lê <46536254+nghiale1@users.noreply.github.com>
2020-03-18 21:12:42 +0700

    #nghia.le updated footer đa ngôn ngữ

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -4,7 +4,8 @@
         <div class="row">
             <div class="col-lg-3 col-md-6 footer-widget">
                 <img src="{{asset('leramiz/img/logo.png')}}" alt="">
-                <p>Batdongsancantho là trang web chuyên kinh doanh bất động sản lớn nhất Cần Thơ.</p>
+                <p>@lang('footer1')</p>
+                <p>@lang('footer2')</p>
                 <div class="social">
                     {{-- <i href="#"><i class="fa fa-facebook"></i></i>
                     <a href="#"><i class="fa fa-twitter"></i></a>
@@ -15,7 +16,7 @@
             </div>
             <div class="col-lg-3 col-md-6 footer-widget">
                 <div class="contact-widget">
-                    <h5 class="fw-title">CONTACT US</h5>
+                    <h5 class="fw-title">@lang('CONTACT US')</h5>
                     <p><i class="fa fa-map-marker"></i>Khu II, đường 3/2, P. Xuân Khánh, Q. Ninh Kiều, TP. Cần Thơ.</p>
                     <p><i class="fa fa-phone"></i>(+12) 345 678 9999</p>
                     <p><i class="fa fa-envelope"></i>realestatect258@gmail.com</p>
@@ -24,29 +25,29 @@
             </div>
             <div class="col-lg-3 col-md-6 footer-widget">
                 <div class="double-menu-widget">
-                    <h5 class="fw-title">POPULAR PLACES</h5>
+                    <h5 class="fw-title">@lang('POPULAR PLACES')</h5>
                     <ul>
-                        <li><a href="">Florida</a></li>
-                        <li><a href="">New York</a></li>
-                        <li><a href="">Washington</a></li>
-                        <li><a href="">Los Angeles</a></li>
-                        <li><a href="">Chicago</a></li>
+                        <li><a href="">Cần Thơ</a></li>
+                        <li><a href="">Hậu Giang</a></li>
+                        <li><a href="">Tiền Giang</a></li>
+                        <li><a href="">Long An</a></li>
+                        <li><a href="">Sóc Trăng</a></li>
                     </ul>
                     <ul>
-                        <li><a href="">St Louis</a></li>
-                        <li><a href="">Jacksonville</a></li>
-                        <li><a href="">San Jose</a></li>
-                        <li><a href="">San Diego</a></li>
-                        <li><a href="">Houston</a></li>
+                        <li><a href="">Hà Nội</a></li>
+                        <li><a href="">Đà Nặng</a></li>
+                        <li><a href="">Nha Trang</a></li>
+                        <li><a href="">Đà Lạt</a></li>
+                        <li><a href="">Hồ Chí Minh</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6  footer-widget">
                 <div class="newslatter-widget">
-                    <h5 class="fw-title">NEWSLETTER</h5>
-                    <p>Subscribe your email to get the latest news and new offer also discount</p>
+                    <h5 class="fw-title">@lang('NEWSLETTER')</h5>
+                    <p>@lang('Subscribe your email to get the latest news and new offer also discount')</p>
                     <form class="footer-newslatter-form">
-                        <input type="text" placeholder="Email address">
+                        <input type="text" placeholder="@lang('Email address')">
                         <button><i class="fas fa-paper-plane" style="color:white"></i></button>
                     </form>
                 </div>
@@ -56,15 +57,15 @@
             <div class="footer-nav">
                 <table style="color:white;">
                     <tr>
-                        <td>Đang trực tuyến </td>
+                        <td>Online </td>
                         <td>&nbsp;&nbsp;&nbsp;&nbsp;: {{ $query_result_person[0]}}</td>
                     </tr>
                     <tr>
-                        <td>Lượt truy cập hôm nay </td>
+                        <td>Visits today </td>
                         <td>&nbsp;&nbsp;&nbsp;&nbsp;: {{ $query_result_person[1]}}</td>
                     </tr>
                     <tr>
-                        <td>Lượt truy cập </td>
+                        <td>Access times </td>
                         <td>&nbsp;&nbsp;&nbsp;&nbsp;: {{ $query_result_person[2]}}</td>
                     </tr>
                 </table>
@@ -74,8 +75,8 @@
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     Copyright &copy;<script>
                         document.write(new Date().getFullYear());
-                    </script> All rights reserved | This template is made with <i class="fa fa-heart-o"
-                        aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    </script> All rights reserved | This template is made with <i class="far fa-heart"></i> by <a
+                        href="https://colorlib.com" target="_blank">Colorlib</a>
 
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
