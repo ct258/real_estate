09948a56730a5a709053fcdfde0cf10cae987820
Nghĩa Lê <46536254+nghiale1@users.noreply.github.com>
2020-04-16 09:58:53 +0700

    #nghia.le thêm intro

--- app/Models/BlogTranslation.php
+++ app/Models/BlogTranslation.php
@@ -17,6 +17,7 @@ class BlogTranslation extends Model
         'blog_translation_id',
         'blog_translation_title',
         'blog_translation_content',
+        'blog_translation_intro',
         'blog_translation_locale',
         'created_at',
         'updated_at',
