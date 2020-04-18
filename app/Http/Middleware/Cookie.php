d4a2591c50045acc3062ac20af614d2d4a344b47
Nghĩa Lê <46536254+nghiale1@users.noreply.github.com>
2020-04-18 14:13:48 +0700

    #nghia.le sửa lại header đa ngôn ngữ

--- app/Http/Middleware/Cookie.php
+++ app/Http/Middleware/Cookie.php
@@ -4,7 +4,11 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Models\CookieUser;
-
+use App\Models\Form;
+use App\Models\FormTranslation;
+use App\Models\Type;
+use App\Models\TypeTranslation;
+use Illuminate\Support\Facades\View;
 class Cookie
 {
     /**
@@ -16,7 +20,20 @@ class Cookie
      */
     public function handle($request, Closure $next)
     {
-        // dd(\Cookie::get('real_estate'));
+        $form=Form::join('form_translation','form.form_id','form_translation.form_id')
+        ->where("form_translation_locale",\Session::get('lang', config('app.locale')))
+        ->get();
+        $type=Type::join('type_translation','type.type_id','type_translation.type_id')
+        ->where("type_translation_locale",\Session::get('lang', config('app.locale')))
+        ->get();
+        $data = array(
+            'form' => $form,
+            'type' => $type,
+        );
+        View::share('data', $data);
+
+
+        
         if(\Cookie::get('real_estate')==null || \Cookie::get('real_estate')==''){
             $id=CookieUser::insertGetId([]);
             \Cookie::queue('real_estate', $id, 43200);
