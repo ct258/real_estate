f606ebc81d69625f26019492c9e88afdd75b039d
Nghia <minhnghia2311@gmail.com>
2020-05-05 19:18:26 +0700

    edit banner

--- app/Models/Providers/AppServiceProvider.php
+++ app/Models/Providers/AppServiceProvider.php
@@ -25,16 +25,10 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        $banner1= DB::table('banner')->where('banner_id',43)->value('banner_title');
-        $banner2= DB::table('banner')->where('banner_id',44)->value('banner_title');
-        $banner3= DB::table('banner')->where('banner_id',45)->value('banner_title');
-        $banner4= DB::table('banner')->where('banner_id',46)->value('banner_title');
-        $banner= array(
-            'banner1'=>$banner1,
-            'banner2'=>$banner2,
-            'banner3'=>$banner3,
-            'banner4'=>$banner4,
-        );
+        $banner= DB::table('banner')->where('banner_type','banner')
+        ->orderBy('banner_id', 'desc')
+        ->limit(4)
+        ->get();
         View::share('banner', $banner);
     }
 }
