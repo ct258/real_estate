dca1f7ea5fae392f6e6344065c946113ccbb7cc7
Nghia <minhnghia2311@gmail.com>
2020-05-08 15:02:56 +0700

    #nghia.le đem thẻ form ra ngoài

--- resources/views/layouts/user/filter.blade.php
+++ resources/views/layouts/user/filter.blade.php
@@ -1,22 +1,22 @@
 <!-- Filter form section -->
 {{-- <div class="container"> --}}
-<div class="row search">
-    <div class="col search1" data-wow-delay="1s">
-        <form action="{{route('list.sort')}}" method="post">
-            @csrf
+<form action="{{route('list.sort')}}" method="post">
+    @csrf
+    <div class="row search">
+        <div class="col search1" data-wow-delay="1s">
             <div class="input-group input-group_1">
                 <input class="form-control" placeholder="@lang('Search_local')" id="search" type="text" value=""
                     name="search">
             </div>
             {{-- </form> --}}
-    </div>
+        </div>
 
-    <div class="col-lg-2 search1" data-wow-delay="1s">
-        <button class="btn btn-orange " type="submit">
-            @lang('Search') <i class="fa fa-search" style="color: white;"></i>
-        </button>
+        <div class="col-lg-2 search1" data-wow-delay="1s">
+            <button class="btn btn-orange " type="submit">
+                @lang('Search') <i class="fa fa-search" style="color: white;"></i>
+            </button>
+        </div>
     </div>
-    </form>
-</div>
-</div>
+    </div>
+</form>
 <!-- Filter form section end -->
