6a80ec0d5e083e5b75308e469a1eb9c9d546eee9
Nghia <minhnghia2311@gmail.com>
2020-05-12 23:36:09 +0700

    #nghia.le phan quyen chi cho khách hàng

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -18,7 +18,9 @@ class CheckLogin
     public function handle($request, Closure $next)
     {
         // nếu user đã đăng nhập
-        if (Auth::guard('account')->check()) {
+        if (Auth::guard('account')->check()&&Auth::guard('account')->user()->hasRole('Customer')) {
+        // if (Auth::guard('account')->user()->hasRole('Customer')) {
+        //     dd(count(Auth::guard('account')->user()->load('customer')->customer));
             $user = Auth::user();
             // nếu level =1 (admin), status = 1 (actived) thì cho qua.
             // if ($user->level == 1 && $user->status == 1) {
